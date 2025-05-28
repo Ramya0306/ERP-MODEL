591b0c50e3807c5a7b23f1bb4a2bb041b76ede97
Ramya0306 <ramya2006senthil@gmail.com>
2025-05-28 13:57:06 +0530

    Go

--- resource-tracker/src/Components/Customer.js
+++ resource-tracker/src/Components/Customer.js
@@ -1,23 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { Card, Table, Form, Input, Button, Row, Col, DatePicker, notification, Spin } from 'antd';
-import axios from 'axios';
-import { LoadingOutlined } from '@ant-design/icons';
-import { getPowerBIUrl } from './powerBiUrls';
+import React, { useState, useEffect } from "react";
+import {
+  Card,
+  Table,
+  Form,
+  Input,
+  Button,
+  Row,
+  Col,
+  DatePicker,
+  notification,
+  Spin,
+} from "antd";
+import axios from "axios";
+import { LoadingOutlined } from "@ant-design/icons";
+import { getPowerBIUrl } from "./powerBiUrls";
 
 const Customer = () => {
-  const [customer, setCustomer] = useState({ name: '', contact: '', email: '', date: '',location:'' });
+  const [customer, setCustomer] = useState({
+    name: "",
+    contact: "",
+    email: "",
+    date: "",
+    location: "",
+  });
   const [customers, setCustomers] = useState([]);
   const [loading, setLoading] = useState(false);
   const [submitting, setSubmitting] = useState(false);
-  const [powerBiUrl, setPowerBiUrlState] = useState(null); 
+  const [powerBiUrl, setPowerBiUrlState] = useState(null);
 
   const fetchCustomers = async () => {
     setLoading(true);
     try {
-      const response = await axios.get('http://192.168.0.89:8080/api/customer');
+      const response = await axios.get("http://192.168.0.89:8080/api/customer");
       setCustomers(response.data);
     } catch (error) {
-      console.error('Error fetching customer data:', error);
+      console.error("Error fetching customer data:", error);
     } finally {
       setLoading(false);
     }
@@ -27,17 +44,27 @@ const Customer = () => {
     e.preventDefault();
     setSubmitting(true);
     try {
-      const response = await axios.post('http://192.168.0.89:8080/api/customer', customer);
+      const response = await axios.post(
+        "http://192.168.0.89:8080/api/customer",
+        customer
+      );
       setCustomers([...customers, response.data]);
-      setCustomer({ name: '', contact: '', email: '', date: '' ,customerId: '',location:''});
-      notification.success({ message: 'Customer added successfully!' });
+      setCustomer({
+        name: "",
+        contact: "",
+        email: "",
+        date: "",
+        customerId: "",
+        location: "",
+      });
+      notification.success({ message: "Customer added successfully!" });
       fetchCustomers();
     } catch (error) {
-      console.error('Error adding customer:', error);
+      console.error("Error adding customer:", error);
       notification.error({
-        message: 'Error',
-        description: 'There was an error. Please try again later.',
-        placement: 'top',
+        message: "Error",
+        description: "There was an error. Please try again later.",
+        placement: "top",
       });
     } finally {
       setSubmitting(false);
@@ -45,12 +72,12 @@ const Customer = () => {
   };
 
   const columns = [
-    { title: 'Name', dataIndex: 'name', key: 'name' },
-    { title: 'Customer ID', dataIndex: 'id', key: 'id' },
-    { title: 'Contact', dataIndex: 'contact', key: 'contact' },
-    { title: 'Email', dataIndex: 'email', key: 'email' },
-    { title: 'Date', dataIndex: 'date', key: 'date' },
-    {title:'Location',dataIndex:'loaction',key:'location'},
+    { title: "Name", dataIndex: "name", key: "name" },
+    { title: "Customer ID", dataIndex: "id", key: "id" },
+    { title: "Contact", dataIndex: "contact", key: "contact" },
+    { title: "Email", dataIndex: "email", key: "email" },
+    { title: "Date", dataIndex: "date", key: "date" },
+    { title: "Location", dataIndex: "loaction", key: "location" },
   ];
 
   const handleDateChange = (date, dateString) => {
@@ -59,61 +86,82 @@ const Customer = () => {
 
   useEffect(() => {
     fetchCustomers();
-    const pieUrl = getPowerBIUrl('customers', 'line');
-    setPowerBiUrlState(pieUrl); 
+    const pieUrl = getPowerBIUrl("customers", "line");
+    setPowerBiUrlState(pieUrl);
   }, []);
 
   return (
-    <div style={{ padding: '20px' }}>
+    <div style={{ padding: "20px" }}>
       <Row gutter={18}>
         <Col span={24}>
-          <Card title="Add Customer" style={{ marginBottom: '20px' }}>
-            <Form layout="inline" onSubmitCapture={handleSubmit}>
-              <Form.Item label="Name">
+          <Card title="Add Customer" style={{ marginBottom: "20px" }}>
+            <Form
+              layout="inline"
+              onSubmitCapture={handleSubmit}
+              style={{
+                display: "flex",
+                gap: "8px",
+                flexWrap: "nowrap",
+                alignItems: "center",
+              }}
+            >
+              <Form.Item label="Name:" style={{ marginBottom: 0 }}>
                 <Input
-                  name="name"
+                  style={{ width: 120 }}
                   value={customer.name}
-                  
-                  placeholder="Name"
-                  onChange={(e) => setCustomer({ ...customer, name: e.target.value })}
+                  onChange={(e) =>
+                    setCustomer({ ...customer, name: e.target.value })
+                  }
                 />
               </Form.Item>
-              <Form.Item label="Contact">
+              <Form.Item label="Contact:" style={{ marginBottom: 0 }}>
                 <Input
-                  name="contact"
-                  
+                  style={{ width: 120 }}
                   value={customer.contact}
-                  placeholder="Contact"
-                  onChange={(e) => setCustomer({ ...customer, contact: e.target.value })}
+                  onChange={(e) =>
+                    setCustomer({ ...customer, contact: e.target.value })
+                  }
                 />
               </Form.Item>
-              <Form.Item label="Email">
+              <Form.Item label="Email:" style={{ marginBottom: 0 }}>
                 <Input
-                  name="email"
-                  
+                  style={{ width: 160 }}
                   value={customer.email}
-                  placeholder="Email"
-                  onChange={(e) => setCustomer({ ...customer, email: e.target.value })}
+                  onChange={(e) =>
+                    setCustomer({ ...customer, email: e.target.value })
+                  }
                 />
               </Form.Item>
-              <Form.Item label="Date">
+              <Form.Item label="Date:" style={{ marginBottom: 0 }}>
                 <DatePicker
-                  style={{ width: '100%' }}
-                  onChange={handleDateChange}
+                  style={{ width: 130 }}
                   format="YYYY-MM-DD"
+                  onChange={handleDateChange}
                 />
-                
               </Form.Item>
-              <Form.Item>
+              <Form.Item label="Location:" style={{ marginBottom: 0 }}>
+                <Input
+                  style={{ width: 130 }}
+                  value={customer.location}
+                  onChange={(e) =>
+                    setCustomer({ ...customer, location: e.target.value })
+                  }
+                />
+              </Form.Item>
+              <Form.Item style={{ marginBottom: 0 }}>
                 <Button type="primary" htmlType="submit" loading={submitting}>
-                  {submitting ? 'Adding...' : 'Add Customer'}
+                  {submitting ? "Adding..." : "Add Customer"}
                 </Button>
               </Form.Item>
             </Form>
           </Card>
 
-          <Card title="Customer Data" style={{ marginBottom: '20px' }}>
-            <Spin spinning={loading} indicator={<LoadingOutlined style={{ fontSize: 24 }} />} tip="Loading Data...">
+          <Card title="Customer Data" style={{ marginBottom: "20px" }}>
+            <Spin
+              spinning={loading}
+              indicator={<LoadingOutlined style={{ fontSize: 24 }} />}
+              tip="Loading Data..."
+            >
               <Table
                 columns={columns}
                 dataSource={customers}
@@ -129,7 +177,7 @@ const Customer = () => {
                 title="Power BI Customer Line Chart"
                 width="1000"
                 height="700"
-                src='https://app.powerbi.com/view?r=eyJrIjoiMTRiZGM3ZjItZGU4Ny00MDI4LWJhNjMtNWRlMTE2M2Y0YTAxIiwidCI6ImNkYmI0MzAwLWFkZDEtNGEwNy1hYjMxLThjZDZmYzBmYjNjMiJ9'
+                src="https://app.powerbi.com/view?r=eyJrIjoiMTRiZGM3ZjItZGU4Ny00MDI4LWJhNjMtNWRlMTE2M2Y0YTAxIiwidCI6ImNkYmI0MzAwLWFkZDEtNGEwNy1hYjMxLThjZDZmYzBmYjNjMiJ9"
                 frameBorder="0"
                 allowFullScreen
               />
